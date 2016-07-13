8360e12dffe863ab838a1294827dbdee3e07a239
Brian Oxley <boxley@thoughtworks.com>
2016-07-13 17:16:37 -0500

    It builds

---
+++ src/main/java/eg/fitnesse/fixture/slim/web/EgTest.java
@@ -0,0 +1,13 @@
+package eg.fitnesse.fixture.slim.web;
+
+import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
+
+public class EgTest
+        extends BrowserTest {
+    public EgTest() {
+    }
+
+    public EgTest(final int secondsBeforeTimeout) {
+        super(secondsBeforeTimeout);
+    }
+}
